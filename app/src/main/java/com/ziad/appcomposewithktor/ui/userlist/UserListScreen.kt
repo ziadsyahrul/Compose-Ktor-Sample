7d439da347fd12b6b7ca4edd212498a3c8a16270
ZIAD-THINKPAD\Ziad Syahrul <ziadsyahrulm@gmail.com>
2026-05-16 17:02:43 +0700

    fix: change appbar text on UserListScreen

--- app/src/main/java/com/ziad/appcomposewithktor/ui/userlist/UserListScreen.kt
+++ app/src/main/java/com/ziad/appcomposewithktor/ui/userlist/UserListScreen.kt
@@ -47,7 +47,7 @@ fun UserListScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Users") },
+                title = { Text("Users App") },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary,
                     titleContentColor = MaterialTheme.colorScheme.onPrimary,
